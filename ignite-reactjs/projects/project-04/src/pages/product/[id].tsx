8d05f061b7abdacf5785b18b98a67d463683a4a6
Felipe Daniel <felipedaniel.me@gmail.com>
2024-04-24 19:22:38 -0300

    fix(ignite-reactjs - project-04): remove id's from paths

--- ignite-reactjs/projects/project-04/src/pages/product/[id].tsx
+++ ignite-reactjs/projects/project-04/src/pages/product/[id].tsx
@@ -64,11 +64,7 @@ export default function Product({ product }: ProductProps) {
 
 export const getStaticPaths: GetStaticPaths = async() => {
   return {
-    paths: [
-      {
-        params: {id: 'prod_Mfx8b7axrbK15n'}
-      }
-    ],
+    paths: [],
     
     fallback: "blocking"
   }
