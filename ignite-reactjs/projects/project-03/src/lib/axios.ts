f4232d5d70e63b304a9dd35e38cf5ab80df307fa
Felipe Daniel <felipedaniel.me@gmail.com>
2024-04-24 22:30:41 -0300

    fix(ignite-reactjs - project-03): fix the database

--- ignite-reactjs/projects/project-03/src/lib/axios.ts
+++ ignite-reactjs/projects/project-03/src/lib/axios.ts
@@ -1,6 +1,5 @@
 import axios from 'axios'
 
 export const api = axios.create({
-  baseURL:
-    'https://my-json-server.typicode.com/feponiel/ignite-courses-vault/tree/main/ignite-reactjs/projects/project-03',
+  baseURL: 'http://localhost:3000',
 })
