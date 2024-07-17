e2304b11ff671bf795f39bfe38142294b22f65b5
Felipe Daniel <felipedaniel.me@gmail.com>
2024-07-16 21:59:22 -0300

    chore: prepare to deploy

--- ignite-reactjs/projects/project-06/src/pages/api/users/[username]/blocked-dates.api.ts
+++ ignite-reactjs/projects/project-06/src/pages/api/users/[username]/blocked-dates.api.ts
@@ -43,22 +43,24 @@ export default async function handle(
 
   const blockedDatesRaw: Array<{ date: number }> = await prisma.$queryRaw`
     SELECT
-      EXTRACT(DAY FROM schedulings.date) AS date,
-      COUNT(schedulings.date) AS amount,
-      ((UTI.time_end_in_minutes - UTI.time_start_in_minutes) / 60) AS size
-
-    FROM schedulings
-
-    LEFT JOIN user_time_intervals UTI
-      ON UTI.week_day = WEEKDAY(DATE_ADD(schedulings.date, INTERVAL 1 DAY))
-
-    WHERE schedulings.user_id = ${user.id}
-      AND DATE_FORMAT(schedulings.date, "%Y-%m") = ${`${year}-${month}`}
-
-    GROUP BY EXTRACT(DAY FROM schedulings.date),
+      EXTRACT(DAY FROM S.DATE) AS date,
+      COUNT(S.date),
       ((UTI.time_end_in_minutes - UTI.time_start_in_minutes) / 60)
 
-    HAVING amount >= size
+    FROM schedulings S
+
+    LEFT JOIN user_time_intervals UTI
+      ON UTI.week_day = EXTRACT(DOW FROM S.date + INTERVAL '1 day')
+
+    WHERE S.user_id = ${user.id}
+      AND EXTRACT(YEAR FROM S.date) = ${year}::int
+      AND EXTRACT(MONTH FROM S.date) = ${month}::int
+
+    GROUP BY EXTRACT(DAY FROM S.DATE),
+      ((UTI.time_end_in_minutes - UTI.time_start_in_minutes) / 60)
+
+    HAVING
+      COUNT(S.date) >= ((UTI.time_end_in_minutes - UTI.time_start_in_minutes) / 60);
   `
 
   const blockedDates = blockedDatesRaw.map((item) => item.date)
